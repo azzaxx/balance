3dd1aa67be00e908f83625ee467d3289527d904d
Алекс <Sagayda4niyAlexey@gmail.com>
2014-10-01 18:57:54 +0300

    Внес некоторые графические изменения. Определил кнопку "Назад" во втором окне.

--- src/com/example/balance/Window2.java
+++ src/com/example/balance/Window2.java
@@ -1,11 +1,30 @@
 package com.example.balance;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class Window2 extends ActionBarActivity {
+public class Window2 extends ActionBarActivity implements OnClickListener {
+	Button back;
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_window2);
+		
+		back = (Button)findViewById(R.id.backInWindow2);
+		back.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		if ( v.getId() == R.id.backInWindow2) {
+			Log.d("MyLog", "backinWindow2 pressed");
+			Intent intent = new Intent(this, Window1.class);
+			startActivity(intent);
+		}
 	}
 }
