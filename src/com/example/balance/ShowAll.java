0c48d8c9388850cc8486b1f0b9497d021cfe5bbe
Алекс <Sagayda4niyAlexey@gmail.com>
2014-10-23 14:50:32 +0300

    Добавил TextView в ShowAll туда вывожу итого теперь. Список больше не подсвечивается при прокрутке.

--- src/com/example/balance/ShowAll.java
+++ src/com/example/balance/ShowAll.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
@@ -19,6 +20,7 @@ import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.BaseAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class ShowAll extends ListActivity {
 	private final String tableName = "MySQLiteTable";
@@ -29,12 +31,15 @@ public class ShowAll extends ListActivity {
 	private SQLiteDatabase db;
 	private Cursor c;
 	private int getKey;
+	private TextView balance;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.custom_list_layout);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		
+		balance = (TextView) findViewById(R.id.textViewBalanceInShowAll);
 	}
 
 	@Override
@@ -79,6 +84,7 @@ public class ShowAll extends ListActivity {
 								public void onClick(DialogInterface dialog,
 										int which) {
 									db.delete(tableName, del, null);
+									total = 0;
 									onResume(); // Вызывается тут для обновления
 												// списка.
 								}
@@ -122,6 +128,8 @@ public class ShowAll extends ListActivity {
 
 		if (c.getCount() == 0) {
 			list.add(new MyList("Записей пока не было..", null, null, null));
+			balance.setText("Баланс: 0.00UAH");
+			balance.setTextColor(Color.RED);
 		} else {
 			String totalTwo;
 			if (total == 0) {
@@ -131,7 +139,13 @@ public class ShowAll extends ListActivity {
 			} else {
 				totalTwo = String.valueOf(total) + ".00UAH";
 			}
-			list.add(new MyList("Баланс:", "Сумма: " + totalTwo, null, null));
+//			list.add(new MyList("Баланс:", "Сумма: " + totalTwo, null, null));
+			balance.setText("Баланс: "+ totalTwo);
+			if (total > 0) {
+				balance.setTextColor(Color.GREEN);
+			} else {
+				balance.setTextColor(Color.RED);
+			}
 		}
 
 		String[] from = { "date", "summ", "notify" };
--- src/com/example/balance/Window1.java
+++ src/com/example/balance/Window1.java
@@ -21,7 +21,7 @@ public class Window1 extends ActionBarActivity implements OnClickListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_window1);
 		
 		editBalance = (Button)findViewById(R.id.button1);
