90c2b6c45c5157cbfcd6255e987d07440762652c
Алекс <Sagayda4niyAlexey@gmail.com>
2014-11-07 16:32:02 +0200

    Добавил в меню выбор отображения категории/месяца. Сделал подсчет/пересчет категорий (получилась гиганская временная сложность, нужно изменять).

--- src/com/example/balance/Window1.java
+++ src/com/example/balance/Window1.java
@@ -47,11 +47,18 @@ public class Window1 extends ListActivity {
 	private String[] month = { "Junaury", "February", "March", "April", "May",
 			"June", "July", "August", "September", "October", "November",
 			"December" };
+	private String[] category = { "Food ", "Road ", "Home ", "Vacation ",
+			"Shop ", "Medecine ", "Car ", "Family ", "Pets ", "Gifts ",
+			"Poker ", "Other" };
+	private TextView[] textViews = { jun, feb, mar, apr, may, june, july, aug,
+			sep, oct, nov, dec };
+	private String displayOptions = "month";
+	private String keyWord = "BALANCE";
 	private long monthCount[] = new long[12];
+	private long categoryCount[] = new long[12];
 	private Button profit;
 	private Button lose;
 	private Intent intent;
-	private String keyWord = "BALANCE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -63,18 +70,18 @@ public class Window1 extends ListActivity {
 		profit = (Button) findViewById(R.id.profit);
 		lose = (Button) findViewById(R.id.lose);
 		balance = (TextView) findViewById(R.id.textViewBalanceInShowAll);
-		jun = (TextView) findViewById(R.id.Junaury);
-		feb = (TextView) findViewById(R.id.February);
-		mar = (TextView) findViewById(R.id.March);
-		apr = (TextView) findViewById(R.id.April);
-		may = (TextView) findViewById(R.id.May);
-		june = (TextView) findViewById(R.id.June);
-		july = (TextView) findViewById(R.id.July);
-		aug = (TextView) findViewById(R.id.August);
-		sep = (TextView) findViewById(R.id.September);
-		oct = (TextView) findViewById(R.id.October);
-		nov = (TextView) findViewById(R.id.November);
-		dec = (TextView) findViewById(R.id.December);
+		textViews[0] = (TextView) findViewById(R.id.Junaury);
+		textViews[1] = (TextView) findViewById(R.id.February);
+		textViews[2] = (TextView) findViewById(R.id.March);
+		textViews[3] = (TextView) findViewById(R.id.April);
+		textViews[4] = (TextView) findViewById(R.id.May);
+		textViews[5] = (TextView) findViewById(R.id.June);
+		textViews[6] = (TextView) findViewById(R.id.July);
+		textViews[7] = (TextView) findViewById(R.id.August);
+		textViews[8] = (TextView) findViewById(R.id.September);
+		textViews[9] = (TextView) findViewById(R.id.October);
+		textViews[10] = (TextView) findViewById(R.id.November);
+		textViews[11] = (TextView) findViewById(R.id.December);
 		// SQLiteDatabase db = myDb.getWritableDatabase();
 		// db.delete("Sagayda4niyAlexeySQLiteTable", null, null);
 
@@ -86,102 +93,21 @@ public class Window1 extends ListActivity {
 				onResume();
 			}
 		});
-		jun.setOnClickListener(new OnClickListener() {
+		for (int i = 0; i < 12; i++) {
+			final int j = i;
+			textViews[i].setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				keyWord = month[0];
-				onResume();
-			}
-		});
-		feb.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				keyWord = month[1];
-				onResume();
-			}
-		});
-		mar.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				keyWord = month[2];
-				onResume();
-			}
-		});
-		apr.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				keyWord = month[3];
-				onResume();
-			}
-		});
-		may.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				keyWord = month[4];
-				onResume();
-			}
-		});
-		june.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				keyWord = month[5];
-				onResume();
-			}
-		});
-		july.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				keyWord = month[6];
-				onResume();
-			}
-		});
-		aug.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				keyWord = month[7];
-				onResume();
-			}
-		});
-		sep.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				keyWord = month[8];
-				onResume();
-			}
-		});
-		oct.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				keyWord = month[9];
-				onResume();
-			}
-		});
-		nov.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				keyWord = month[10];
-				onResume();
-			}
-		});
-		dec.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				keyWord = month[11];
-				onResume();
-			}
-		});
+				@Override
+				public void onClick(View v) {
+					if (displayOptions.equals("month")) {
+						keyWord = month[j];
+					} else {
+						keyWord = category[j];
+					}
+					onResume();
+				}
+			});
+		}
 		profit.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -227,6 +153,7 @@ public class Window1 extends ListActivity {
 		total = 0;
 		for (int i = 0; i < month.length; i++) {
 			monthCount[i] = 0;
+			categoryCount[i] = 0;
 		}
 		list = new ArrayList<MyList>();
 
@@ -295,7 +222,6 @@ public class Window1 extends ListActivity {
 			int getSumm = c.getColumnIndex("summ");
 			int getNotify = c.getColumnIndex("notify");
 			int getMonth = c.getColumnIndex("month");
-			int getYear = c.getColumnIndex("year");
 			int getCategory = c.getColumnIndex("category");
 			getKey = c.getColumnIndex("key");
 
@@ -304,8 +230,13 @@ public class Window1 extends ListActivity {
 				String b = a.substring(1);
 				int d = Integer.parseInt(b);
 				String month = c.getString(getMonth);
+				String cat = c.getString(getCategory);
 
-				Count(d, month, c.getString(getKey));
+				if (displayOptions.equals("month")) {
+					CountMonth(d, month, c.getString(getKey));
+				} else {
+					CountCategory(d, cat, c.getString(getKey));
+				}
 
 				if (keyWord.equals("BALANCE")) {
 					list.add(new MyList(c.getString(getDate), "Sum: "
@@ -319,6 +250,17 @@ public class Window1 extends ListActivity {
 						total -= d;
 					}
 				} else if (keyWord.equals(month)) {
+					list.add(new MyList(c.getString(getDate), "Sum: "
+							+ c.getString(getSumm) + ".00$", "Note: "
+							+ c.getString(getNotify) + "\nCategory: "
+							+ c.getString(getCategory), c.getString(getKey),
+							null, null, null));
+					if (c.getString(getSumm).charAt(0) == (char) 43) {
+						total += d;
+					} else {
+						total -= d;
+					}
+				} else if (haveSome(keyWord, cat)) {
 					list.add(new MyList(c.getString(getDate), "Sum: "
 							+ c.getString(getSumm) + ".00$", "Note: "
 							+ c.getString(getNotify) + "\nCategory: "
@@ -369,77 +311,71 @@ public class Window1 extends ListActivity {
 		super.onResume();
 	}
 
-	private void setText() {
-		if (monthCount[0] != 0) {
-			jun.setText("Junaury " + monthCount[0] + ".00$");
-		} else {
-			jun.setText("Junaury");
+	private boolean haveSome(String keyWord2, String cat) {
+		if (cat.equals(keyWord2)) {
+			return true;
 		}
-		if (monthCount[1] != 0) {
-			feb.setText("February " + monthCount[1] + ".00$");
-		} else {
-			feb.setText("February");
+		for (int i = 0; i < 12; i++) {
+			String s = cat;
+			if (s.length() >= category[i].length()) {
+				s = cat.substring(0, category[i].length());
+			}
+			if (s.equals(keyWord2)){
+				return true;
+			} else if (s.equals(category[i])) {
+				cat = cat.substring(category[i].length());
+			}
 		}
-		if (monthCount[2] != 0) {
-			mar.setText("March " + monthCount[2] + ".00$");
-		} else {
-			mar.setText("March");
-		}
-		if (monthCount[3] != 0) {
-			apr.setText("April " + monthCount[3] + ".00$");
-		} else {
-			apr.setText("April");
-		}
-		if (monthCount[4] != 0) {
-			may.setText("May " + monthCount[4] + ".00$");
-		} else {
-			may.setText("May");
-		}
-		if (monthCount[5] != 0) {
-			june.setText("June " + monthCount[5] + ".00$");
-		} else {
-			june.setText("June");
-		}
-		if (monthCount[6] != 0) {
-			july.setText("July " + monthCount[6] + ".00$");
-		} else {
-			july.setText("July");
-		}
-		if (monthCount[7] != 0) {
-			aug.setText("August " + monthCount[7] + ".00$");
-		} else {
-			aug.setText("August");
-		}
-		if (monthCount[8] != 0) {
-			sep.setText("September " + monthCount[8] + ".00$");
-		} else {
-			sep.setText("September");
-		}
-		if (monthCount[9] != 0) {
-			oct.setText("October " + monthCount[9] + ".00$");
-		} else {
-			oct.setText("October");
-		}
-		if (monthCount[10] != 0) {
-			nov.setText("November " + monthCount[10] + ".00$");
-		} else {
-			nov.setText("November");
-		}
-		if (monthCount[11] != 0) {
-			dec.setText("December " + monthCount[11] + ".00$");
-		} else {
-			dec.setText("December");
-		}
-
+		return false;
 	}
 
-	private void Count(int d, String month1, String key) {
+	private void CountCategory(int d, String cat1, String key) {
+		String s = cat1;
+		for (int i = 0; i < category.length && s.length() > 0; i++) {
+			if (s.length() >= category[i].length()) {
+				s = cat1.substring(0, category[i].length());
+			}
+			if (s.equals(category[i])) {
+				cat1 = cat1.substring(category[i].length());
+				if (key.equals("+")) {
+					categoryCount[i] += d;
+				} else {
+					categoryCount[i] -= d;
+				}
+			}
+			s = cat1;
+		}
+	}
+
+	private void CountMonth(int d, String month1, String key) {
 		for (int i = 0; i < month.length; i++) {
 			if (month[i].equals(month1)) {
 				if (key.equals("+")) {
 					monthCount[i] += d;
 				} else {
 					monthCount[i] -= d;
+				}
+			}
+		}
+	}
+
+	private void setText() {
+		if (displayOptions.equals("month")) {
+			for (int i = 0; i < 12; i++) {
+				if (monthCount[i] != 0) {
+					textViews[i].setText(month[i] + " " + monthCount[i]
+							+ ".00$");
+				} else {
+					textViews[i].setText(month[i]);
+				}
+			}
+		} else {
+			for (int i = 0; i < 12; i++) {
+				if (categoryCount[i] != 0) {
+					textViews[i].setText(category[i] + " " + categoryCount[i]
+							+ ".00$");
+				} else {
+					textViews[i].setText(category[i]);
 				}
 			}
 		}
@@ -454,49 +390,30 @@ public class Window1 extends ListActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(Window1.this);
-		LayoutInflater inflater = Window1.this.getLayoutInflater();
-		View vw = inflater.inflate(R.layout.two_button_lyaout, null);
-		RadioGroup rg = (RadioGroup) vw.findViewById(R.id.radioGroup);
-		RadioButton showMonth = (RadioButton) vw.findViewById(R.id.showMonth);
-		RadioButton showCategory = (RadioButton) vw
-				.findViewById(R.id.showCategory);
-
-		onRadioButtonClicked(rg);
-		builder.setView(inflater.inflate(R.layout.two_button_lyaout, null))
-				.setPositiveButton("Cancel",
-						new DialogInterface.OnClickListener() {
-							@Override
-							public void onClick(DialogInterface dialog, int id) {
-								dialog.cancel();
-							}
-						})
-				.setNegativeButton("Confirm",
-						new DialogInterface.OnClickListener() {
-							@Override
-							public void onClick(DialogInterface dialog, int id) {
-								dialog.cancel();
-							}
-						});
-		AlertDialog alert = builder.create();
-		alert.show();
-		return super.onOptionsItemSelected(item);
-	}
-
-	public void onRadioButtonClicked(View view) {
-		Log.d("MyLog", "--------------show 11111 on");
-
-		switch (view.getId()) {
-		case R.id.radioGroup:
-			Log.d("MyLog", "radioGroupradioGroupradioGroupradioGroup month on");
-		break;
-		case R.id.showMonth:
-				Log.d("MyLog", "show month on");
-			break;
-		case R.id.showCategory:
-				Log.d("MyLog", "show category on");
-			break;
+		if (item.getItemId() == R.id.showMonth) {
+			displayOptions = "month";
+			Log.d("MyLog", "show month on!!!!");
+			onResume();
+		} else if (item.getItemId() == R.id.showCategory) {
+			displayOptions = "category";
+			Log.d("MyLog", "show category on!!!!");
+			onResume();
+		} else if (item.getItemId() == R.id.about) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(Window1.this);
+			LayoutInflater inflater = Window1.this.getLayoutInflater();
+			builder.setView(inflater.inflate(R.layout.two_button_lyaout, null))
+					.setPositiveButton("Close",
+							new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface dialog,
+										int id) {
+									dialog.cancel();
+								}
+							});
+			AlertDialog alert = builder.create();
+			alert.show();
 		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
